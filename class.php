f7034935eb89848b7a9b82537685b451e1f93471
MarkAguirre26 <markaguirre26@gmail.com>
2018-02-09 16:10:48 +0800

    Initial commit

---
+++ class.php
@@ -0,0 +1,46 @@
+<?php 
+include_once("../config/config.php");
+
+class MySqlClass 
+{
+	function getData($query){
+		global $con;
+		$result = mysqli_query($con,$query);
+			if($result -> num_rows > 0)
+			{
+				while ($row = mysqli_fetch_assoc($result)) 
+				{
+					$data[] = $row;
+				}
+				//echo json_encode($data);	
+			}
+			else {
+				$data = "No Data";
+			}
+		mysqli_close($con);
+	return $data;
+	}
+
+	function executeQuery($query){
+		global $con;
+		$result = mysqli_query($con,$query);
+			if($result)	{
+				$data =  "True";
+			}
+			else 	{
+				$data = "False";
+			}
+		mysqli_close($con);
+	return $data;
+	}
+
+	public function fetchQuery($select) {
+		global $con;
+		$query = mysqli_query($con, $select);
+		return $query;
+	}
+
+
+}
+
+?>
---
+++ init.php
@@ -0,0 +1,3 @@
+<?php
+$con = mysqli_connect("localhost", "id3789367_localhost", "password", "id3789367_peso_db");
+?>
